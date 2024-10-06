b5a0b439cc5e11884d3e91b7ff7293b3b00730c8
Sam Lamboo <125293490+NerdyN00B@users.noreply.github.com>
2024-10-06 12:40:13 +0200

    formatting

--- PE1/session_3/prepsketch.py
+++ PE1/session_3/prepsketch.py
@@ -12,8 +12,10 @@ def output_fraction(f, cap, res):
     x_c= 1 / (2 * np.pi * f * cap)
     return res / np.sqrt(res**2 + x_c**2)
 
+
 def cutoff(res, cap):
     return 1 / (2 * np.pi * res * cap)
+
 
 fig, ax = plt.subplots(dpi=300, layout='tight')
 
@@ -23,15 +25,16 @@ ax.semilogx(f, ydata, c='k', label='transfer function')
 ax.vlines(cutoff(res, cap), np.min(ydata), 0, colors='r', linestyles='dashed',
           label='Cutoff frequency')
 ax.hlines(-3, 0, np.max(f), colors='r', linestyles='dashed')
-ax.annotate(f'Cutoff frequency {cutoff(res, cap):.2f} Hz',
+ax.annotate(f'Cutoff frequency {cutoff(res, cap):.0f} Hz',
             xy=(cutoff(res, cap), -3),
             xytext=(-100, 20),
             textcoords='offset points',
             arrowprops=dict(facecolor='black', arrowstyle='->'))
+
 ax.set_yticks(np.append(ax.get_yticks(), -3))
 ax.set_xlabel('Frequency [Hz]')
 ax.set_ylabel('Gain [dB]')
-ax.grid()
 ax.set_title('transfer function for high-pass filter')
+
+ax.grid()
 fig.savefig('PE1/session_3/highpass.pdf')
-plt.show()
