620649be3c0a4d1a97ad4dd3fc4dd3352dc9517f
Sam Lamboo <125293490+NerdyN00B@users.noreply.github.com>
2024-11-18 23:44:31 +0100

    refactored mydaq class
    Made mydaq class do all analysis functions

--- PE1/session_5/mydaq.py
+++ PE1/session_5/mydaq.py
@@ -4,8 +4,8 @@ This module provides a class to control the MyDAQ. Specifically allowing the
 user to read and write data to the MyDAQ as both seperate tasks, and
 simultaneously.
 
-Parts of this code were inspired by code from Stan, provided by 
-Leiden University's PE1 course.
+Parts of this code were inspired or modified from the mydaqclass code made by 
+Stan, provided by Leiden University's PE1 course.
 
 Author: Sam Lamboo
 Institution: Leiden University
@@ -16,7 +16,8 @@ import numpy as np
 import nidaqmx as dx
 from time import sleep
 from scipy.signal import sawtooth, square
-
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 class MyDAQ():
     """A class to controll the MyDAQ"""
@@ -210,10 +211,8 @@ class MyDAQ():
         samps = MyDAQ.convertDurationToSamples(rate, duration)
 
         with dx.Task('readTask') as readTask:
-            readTask.ai_channels.add_ai_voltage_chan(f'{self.name}/{channel}')
-
-            readTask.timing.cfg_samp_clk_timing(rate, sample_mode=self.finite,
-                                                samps_per_chan=samps)
+            self._addInputChannels(readTask, channel)
+            self._configureChannelTimings(readTask, samps)
 
             read_data = readTask.read(number_of_samples_per_channel = samps)
             return np.asarray(read_data)
@@ -241,11 +240,306 @@ class MyDAQ():
             
             if samps is None:
                 samps = len(write_data)
-            writeTask.ao_channels.add_ao_voltage_chan(f'{self.name}/{channel}')
-            writeTask.timing.cfg_samp_clk_timing(rate, sample_mode = dx.constants.AcquisitionType.FINITE, samps_per_chan=samps)
+            
+            self._addOutputChannels(writeTask, channel)
+            self._configureChannelTimings(writeTask, samps)
+            
             writeTask.write(write_data, auto_start=True)
             sleep(samps/rate + 0.001)
             writeTask.stop()
+    
+    def measure_spectrum(self,
+                         frequencies: list[int]|np.ndarray,
+                         duration: float =1,
+                         amplitude: float =3,
+                         repeat: int =1,
+                         write_channel: str ='ao0',
+                         read_input_channel: str ='ai0',
+                         read_output_channel: str ='ai1',
+                         ):
+        """Measure over a spectrum of frequencies.
+        
+        parameters
+        ----------
+        frequencies : np.ndarray
+            The frequencies to measure at
+        duration : float
+            The duration of the measurement per frequency
+        amplitude : float
+            The amplitude of the waveform
+        repeat : int
+            The number of times to measure per frequency
+        write_channel : str
+            The channel to write the waveform to
+        read_input_channel : str
+            The channel to read the original input from (straight from the 
+            output to this channel for later comparison)
+        read_output_channel : str
+            The channel to read the output of the system from
+        
+        returns
+        -------
+        np.ndarray
+            The measured data,
+            on the first axis, index 0 is the input data, index 1 is the output
+            data. On the second axis, the index corresponds to repeat. On the
+            third axis, the index corresponds to the frequency as provided.
+        """
+        assert isinstance(repeat, int), "Repeat should be an integer."
+        assert repeat > 0, "Repeat should be a positive integer."
+        
+        input_data = []
+        output_data = []
+        
+        for _ in range(repeat):
+            input_data_i = []
+            output_data_i = []
+            
+            for frequency in frequencies:
+                waveform = self.generateWaveform('sine',
+                                                 self.samplerate,
+                                                 frequency,
+                                                 amplitude,
+                                                 duration=duration
+                                                 )[1]
+                
+                read = self.readWrite(waveform,
+                                      read_channel=[read_input_channel,
+                                                    read_output_channel],
+                                      write_channel=write_channel
+                                      )
+                
+                input_data_i.append(read[0])
+                output_data_i.append(read[1])
+            
+            input_data.append(np.asarray(input_data_i))
+            output_data.append(np.asarray(output_data_i))
+        
+        if repeat == 1:
+            return np.stack((np.asarray(input_data_i),
+                             np.asarray(output_data_i)))
+        else:
+            return np.stack((np.asarray(input_data),
+                             np.asarray(output_data)))
+
+    @staticmethod
+    def get_transfer_functions(
+        data: np.ndarray,
+        frequencies: list[int]|np.ndarray,
+        repeat: int = 1,
+        samplerate: int = 200_000,
+        integration_range: int = 5,
+        ) -> np.ndarray:
+        """Analyse the spectrum of a measured dataset.
+        
+        parameters
+        ----------
+        data : np.ndarray
+            The measured data, like provided by measure_spectrum
+        frequencies : np.ndarray
+            The frequencies measured at
+        repeat : int
+            The number of times the measurement was repeated
+        samplerate : int
+            The samplerate of the measurement
+        integration_range : int
+            How many points to integrate over left and right from the assumed
+            peak. [idx-integratin_range:idx+integration_range] is integrated.
+        
+        returns
+        -------
+        np.ndarray
+            The transfer function(s) of the system
+        """
+        assert isinstance(repeat, int), "Repeat should be an integer."
+        assert repeat > 0, "Repeat should be a positive integer."
+        
+        full_transfer = []
+        for i in range(repeat):
+            transfer_function = []
+            for j, frequency in enumerate(frequencies):
+                fourier_in = np.fft.fft(data[0][i][j])
+                fourier_out = np.fft.fft(data[1][i][j])
+                freq = np.fft.fftfreq(len(data[0][j]), 1/samplerate)
+                
+                idx = MyDAQ.find_nearest_idx(freq, frequency)
+                integrated_in = np.trapz(fourier_in[idx-integration_range:idx+integration_range],
+                                        freq[idx-integration_range:idx+integration_range])
+                
+                integrated_out = np.trapz(fourier_out[idx-integration_range:idx+integration_range],
+                                        freq[idx-integration_range:idx+integration_range])
+                
+                transfer = integrated_out / integrated_in
+                
+                transfer_function.append(transfer)
+            
+            full_transfer.append(np.asarray(transfer_function))
+        
+        if repeat == 1:
+            return np.asarray(transfer_function)
+        else:
+            return np.asarray(full_transfer)
+        
+    @staticmethod
+    def analyse_transfer(transfer_functions: np.ndarray, gain=True):
+        """Analyse the transfer functions of a system.
+        
+        parameters
+        ----------
+        transfer_functions : np.ndarray
+            The transfer functions of the system
+        gain : bool
+            Whether to analyse the gain or the magnitude
+        
+        returns
+        -------
+        mean_gain/magnitude : np.ndarray
+            The mean gain/magnitude of the transfer functions
+        std_gain/magnitude : np.ndarray
+            The standard deviation of the gain/magnitude of the transfer functions
+        mean_phase : np.ndarray
+            The mean phase of the transfer functions in radians
+        std_phase : np.ndarray
+            The standard deviation of the phase of the transfer functions in
+            radians
+        """
+        magnitude = np.abs(transfer_functions)
+        gain = 20 * np.log10(magnitude)
+        phase = np.angle(transfer_functions)
+        
+        mean_magnitude = np.mean(gain, axis=0)
+        std_magnitude = np.std(gain, axis=0)
+        
+        mean_gain = np.mean(gain, axis=0)
+        std_gain = np.std(gain, axis=0)
+        
+        mean_phase = np.mean(phase, axis=0)
+        std_phase = np.std(phase, axis=0)
+        
+        if gain:
+            return mean_gain, std_gain, mean_phase, std_phase
+        else:
+            return mean_magnitude, std_magnitude, mean_phase, std_phase
+    
+    @staticmethod
+    def make_bodeplot(**kwargs):
+        """Create a bodeplot figure.
+        
+        parameters
+        ----------
+        **kwargs
+            Additional keyword arguments for plt.figure
+        
+        returns
+        -------
+        fig : plt.Figure
+            The figure
+        gain_ax : plt.Axes
+            The axis for the gain plot
+        phase_ax : plt.Axes
+            The axis for the phase plot
+        polar_ax : plt.Axes
+            The axis for the polar plot
+        """
+        fig = plt.figure(**kwargs)
+        gs = gridspec.GridSpec(2, 2, figure=fig)
+        
+        gain_ax = fig.add_subplot(gs[0, 0])
+        phase_ax = fig.add_subplot(gs[1, 0])
+        polar_ax = fig.add_subplot(gs[:, 1], projection='polar')
+        
+        return fig, gain_ax, phase_ax, polar_ax
+        
+    
+    @staticmethod
+    def plot_gain(ax: plt.Axes,
+                  frequencies,
+                  gain,
+                  gain_error,
+                  fmt = 'ok',
+                  capsize = 2,
+                  label = 'mean gain $\pm 2\sigma$',
+                  **kwargs) -> None:
+        """Plot the gain of a system.
+        
+        parameters
+        ----------
+        ax : plt.Axes
+            The axis to plot on
+        frequencies : np.ndarray
+            The frequencies measured at
+        gain : np.ndarray
+            The gain of the transfer function
+        gain_error : np.ndarray
+            The error on the gain of the transfer function
+        **kwargs
+            Additional keyword arguments for ax.errorbar function
+        """
+        ax.errorbar(frequencies, gain, yerr=gain_error, fmt=fmt,
+                    label=label, capsize=capsize, **kwargs)
+        
+        ax.set_xscale('log')
+        ax.set_ylabel('Gain [dB]')
+        ax.set_xlabel('Frequency [Hz]')
+        ax.set_title('Gain transfer function')
+    
+    @staticmethod
+    def plot_phase(ax: plt.Axes,
+                   frequencies,
+                   phase,
+                   phase_error,
+                   deg=True,
+                   fmt = 'ok',
+                   capsize = 2,
+                   label = 'mean phase $\pm 2\sigma$',
+                   **kwargs) -> None:
+        """Plot the phase of a system.
+        
+        parameters
+        ----------
+        ax : plt.Axes
+            The axis to plot on
+        frequencies : np.ndarray
+            The frequencies measured at
+        phase : np.ndarray
+            The phase of the transfer function
+        phase_error : np.ndarray
+            The error on the phase of the transfer function
+        deg : bool
+            Whether to convert the phase to degrees
+        **kwargs
+            Additional keyword arguments for ax.errorbar function
+        """
+        if deg:
+            phase = np.rad2deg(phase)
+            phase_error = np.rad2deg(phase_error)
+        
+        ax.errorbar(frequencies, phase, yerr=phase_error, fmt=fmt,
+                    label=label, capsize=capsize, **kwargs)
+        
+        ax.set_xscale('log')
+        ax.set_ylabel('Phase [°]')
+        ax.set_xlabel('Frequency [Hz]')
+        ax.set_title('Phase transfer function')
+    
+    @staticmethod
+    def plot_polar(ax: plt.Axes,
+                   gain: np.ndarray,
+                   phase: np.ndarray,
+                   magnitude=False,
+                   ):
+        """Make polar plot of transfer function.
+        
+        if magnitude is True, gain is interpreted as magnitude. Otherwise gain
+        is interpreted as dB and magnitude is calculated from gain.
+        """
+        if not magnitude:
+            magnitude = 10**(gain/20)
+        else: 
+            magnitude = gain
+        
+        ax.plot(phase, magnitude)
+        ax.set_title('Polar plot of transfer function')
 
     @staticmethod
     def generateWaveform(
